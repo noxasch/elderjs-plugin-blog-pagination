d2a8374ab1688bab7007891445f73a284acf9cb1
Alexander Dischberg <noxasch@gmail.com>
2021-06-17 19:37:02 +0800

    fix: default route and template should be empty
    resolves #5

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const createRouteList = require('./utils/createRouteList');
-
 const plugin = {
   name: 'elderjs-plugin-blog-pagination',
   description: `Generate pagination from markdown blog post`,
@@ -12,27 +10,33 @@ const plugin = {
       name: 'AddIndexPaginationRequest',
       description: 'Generate pagination request object from markdown data',
       run: ({ plugin, data, allRequests, settings }) => {
+        const createRouteList = require('./utils/createRouteList');
         const hasPluginMarkdown = '@elderjs/plugin-markdown' in settings.plugins;
         const postPerPage = plugin.config.postPerPage;
         const routesList = plugin.config.routes;
         const template = plugin.config.indexTemplate;
 
-        if (hasPluginMarkdown) {
+        if (hasPluginMarkdown && plugin.config.routes.length > 0 && plugin.config.indexTemplate) {
           routesList.forEach((route) => {
             const slugList = createRouteList(data, postPerPage, route, template);
             allRequests = [...allRequests, ...slugList];
           });
         } else {
-          console.log('elderjs-plugin-blog-pagination: Skipping pagination as @elderjs/plugin-markdown not detected in elderjs plugin.');
-        }
+          if (!hasPluginMarkdown)
+            console.log('elderjs-plugin-blog-pagination: Skipping pagination as @elderjs/plugin-markdown not detected in elderjs plugin.');
+          if (plugin.config.routes.length > 0)
+            console.log('elderjs-plugin-blog-pagination: No routes specified');
+          if (plugin.config.indexTemplate)
+            console.log(`elderjs-plugin-blog-pagination: No template specified.`);
+        } 
         return { allRequests };
       }
     },
   ],
   config: {
-    routes: ['blog'],
+    routes: [],
     postPerPage: 5,
-    indexTemplate: 'BlogIndex'
+    indexTemplate: '',
   },
 
 };
